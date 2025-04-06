6dc8efa3c94c0c48dc1c3a4337f6b1206c972b66
ahmed soumri <ahmedsoumri01@gmail.com>
2025-04-07 00:27:56 +0100

    initialise backend

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db.js");
+const path = require("path");
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+// Initialize Express app
+const app = express();
+// Enable CORS for all requests
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    credentials: true,
+  })
+);
+
+// Serve static files (profile images)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+// Define a simple route for testing
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Set the port from environment variables or default to 3001
+const PORT = process.env.PORT || 3001;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
